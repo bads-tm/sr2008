33f2bb51a6bc1badbfb08668fd9da3c7193bd1c3
Darius Bads <darius.bads@outlook.com>
2017-06-03 07:17:42 +0300

    302 MB (317.710.336 bytes)
    Very big file server useage, atleast someone would fork this, and make
    useable... R1

---
+++ README.md
@@ -0,0 +1,2 @@
+# Stormregion Website 2005 - 2008
+Unofficial version / copy of web archive without it's additions. just like the original... but improved.
---
+++ index.php%3fsid%3d60112d9bbeb90b36678ebc8b8ae4e77b%26fileID%3d183%26activePage%3ddownloadSection.php
@@ -0,0 +1,233 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"><html><head><title>StormRegion.com</title><link rel='stylesheet' href='style-sheet/style.css' type='text/css'><link rel='shortcut icon' href='style-sheet/favicon.ico'><meta HTTP-EQUIV='content-type' CONTENT='text/html; charset=iso-8859-1'><script language='JavaScript1.2'>history.forward(1)</script></head><body style='margin: 0px;background-image: url(./templates/base/images/bgTile.gif)'><center><table cellpadding='0' cellspacing='0' border='0' width='1000' style='height:100%' bgcolor='#000000'><tr><td width='1000' height='119' colspan='3'><map name="map1">
+<area shape="rect" coords="0,0,160,20"
+  href ="index.php?sid=b5fcfdf7c2d78064ee81b7ce72b45be2&amp;activeMenuID=-1&amp;activeProjectID=-1&amp;rulerReset=1" alt='' title=''>
+</map>
+<map name="map2">
+<area shape ="rect" coords="0,0,160,18"
+  href ="index.php?sid=b5fcfdf7c2d78064ee81b7ce72b45be2&amp;activeMenuID=-1&amp;activeProjectID=-1&amp;rulerReset=1" alt='' title=''>
+</map>
+<map name="map3">
+<area shape ="rect" coords="0,0,140,19"
+  href ="index.php?sid=b5fcfdf7c2d78064ee81b7ce72b45be2&amp;activeMenuID=-1&amp;activeProjectID=-1&amp;rulerReset=1" alt='' title=''>
+</map>
+<map name="map4">
+<area shape ="rect" coords="0,0,110,19"
+  href ="index.php?sid=b5fcfdf7c2d78064ee81b7ce72b45be2&amp;activeMenuID=-1&amp;activeProjectID=-1&amp;rulerReset=1" alt='' title=''>
+</map>
+<table width="1000"  height='144' border="0" cellpadding="0" cellspacing="0">
+	<tr style='background-image: url(./templates/base/images/header/index_02.gif); background-repeat: repeat-x;' valign='top' height='55'>
+		<td width="322">
+			<a href='index.php?sid=b5fcfdf7c2d78064ee81b7ce72b45be2&amp;activeMenuID=-1&amp;activeProjectID=-1&amp;rulerReset=1'><img src="./templates/base/images/header/index_01.gif" width="322" height="55" alt="" border="0"></a></td>
+		<td align='left' valign='bottom'><table border="0" cellpadding="0" cellspacing="0" height='17'><tr height='15'><td>
+			<div style='display:none;z-index:0;'><ul id='imenus0'><li><a>Games</a><ul style='width:140px;'><li><a href='index.php?sid=b5fcfdf7c2d78064ee81b7ce72b45be2&amp;activeMenuID=rftb&amp;activeProjectID=109'>Rush&nbsp;for&nbsp;the&nbsp;Bomb</a><li><a href='index.php?sid=b5fcfdf7c2d78064ee81b7ce72b45be2&amp;activeMenuID=rfb&amp;activeProjectID=104'>Rush&nbsp;For&nbsp;Berlin</a><li><a href='index.php?sid=b5fcfdf7c2d78064ee81b7ce72b45be2&amp;activeMenuID=cpph2&amp;activeProjectID=101'>Codename:&nbsp;Panzers&nbsp;Phase&nbsp;Two</a><li><a href='index.php?sid=b5fcfdf7c2d78064ee81b7ce72b45be2&amp;activeMenuID=cpph1&amp;activeProjectID=100'>Codename:&nbsp;Panzers&nbsp;Phase&nbsp;One</a><li><a href='index.php?sid=b5fcfdf7c2d78064ee81b7ce72b45be2&amp;activeMenuID=swine&amp;activeProjectID=103'>S.W.I.N.E.</a></ul></li><li><a>Stormregion&nbsp;Info</a><ul style='width:140px;'><li><a href='index.php?sid=b5fcfdf7c2d78064ee81b7ce72b45be2&amp;activeMenuID=about'>About&nbsp;Stormregion</a><li><a href='index.php?sid=b5fcfdf7c2d78064ee81b7ce72b45be2&amp;activeMenuID=contact'>Contact&nbsp;Stormregion</a><li><a href='index.php?sid=b5fcfdf7c2d78064ee81b7ce72b45be2&amp;activeMenuID=job'>Jobs&nbsp;at&nbsp;Stormregion</a><li><a href='index.php?sid=b5fcfdf7c2d78064ee81b7ce72b45be2&amp;activeMenuID=award&amp;activeProjectID=-1'>Media&nbsp;/&nbsp;Awards</a><li><a href='index.php?sid=b5fcfdf7c2d78064ee81b7ce72b45be2&amp;activeMenuID=press'>Press</a></ul></li><li><a href='index.php?sid=b5fcfdf7c2d78064ee81b7ce72b45be2&amp;activeMenuID=downloads&amp;activeProjectID=-1'>Downloads</a><ul style='width:140px;'><li><a href='index.php?sid=b5fcfdf7c2d78064ee81b7ce72b45be2&amp;activeMenuID=dlrftb&amp;activeProjectID=109&amp;activeMenuID=rftb&amp;activeSubMenuID=rftb_download'>Rush&nbsp;for&nbsp;the&nbsp;Bomb</a><li><a href='index.php?sid=b5fcfdf7c2d78064ee81b7ce72b45be2&amp;activeMenuID=dlrfb&amp;activeProjectID=104&amp;activeMenuID=rfb&amp;activeSubMenuID=rfb_download'>Rush&nbsp;For&nbsp;Berlin</a><li><a href='index.php?sid=b5fcfdf7c2d78064ee81b7ce72b45be2&amp;activeMenuID=dlcpph2&amp;activeProjectID=101&amp;activeMenuID=cpph2&amp;activeSubMenuID=cpph2_download'>Codename:&nbsp;Panzers&nbsp;Phase&nbsp;Two</a><li><a href='index.php?sid=b5fcfdf7c2d78064ee81b7ce72b45be2&amp;activeMenuID=dlcpph1&amp;activeProjectID=100&amp;activeMenuID=cpph1&amp;activeSubMenuID=cpph1_download'>Codename:&nbsp;Panzers&nbsp;Phase&nbsp;One</a><li><a href='index.php?sid=b5fcfdf7c2d78064ee81b7ce72b45be2&amp;activeMenuID=dlswine&amp;activeProjectID=103&amp;activeMenuID=swine&amp;activeSubMenuID=swine_download'>S.W.I.N.E.</a></ul></li><li><a href='forum/index.php?sid=b5fcfdf7c2d78064ee81b7ce72b45be2&amp;'>Forum</a></li><li><a>&nbsp;&nbsp;Mods</a><ul style='width:140px;'><li><a href='index.php?sid=b5fcfdf7c2d78064ee81b7ce72b45be2&amp;activeMenuID=mods2&amp;activeProjectID=104&amp;activeMenuID=rfb&amp;activeSubMenuID=mod2'>Rush&nbsp;For&nbsp;the&nbsp;Bomb</a><li><a href='index.php?sid=b5fcfdf7c2d78064ee81b7ce72b45be2&amp;activeMenuID=mods&amp;activeProjectID=101&amp;activeMenuID=cpph2&amp;activeSubMenuID=mod'>Codename:&nbsp;Panzers&nbsp;Phase&nbsp;Two</a><li><a href='index.php?sid=b5fcfdf7c2d78064ee81b7ce72b45be2&amp;activeMenuID=mods2&amp;activeProjectID=104&amp;activeMenuID=rfb&amp;activeSubMenuID=mod2'>Rush&nbsp;For&nbsp;Berlin</a><script type="text/javascript" src="menu/imenus_data.js"></script><script type="text/javascript" src="menu/imenus_code.js"></script>
+		</td></tr><tr height='2'><td></td></tr></table>
+		</td>
+	</tr>
+	<tr style='background-image: url(./templates/base/images/header/index_07.gif); background-repeat: repeat-x;' valign='top' height='62'>
+		<td colspan='2'>
+		<table width="1000" height='62' border="0" cellpadding="0" cellspacing="0">
+		<tr valign='bottom'><td rowspan="2" width="169" height="62" ><img src="./templates/base/images/header/index_04.gif" width="169" height="62" alt=""></td>
+		<td width="74" height="44" colspan="2" style='background-image: url(./templates/base/images/header/index_06.gif); background-position:top left; background-repeat: no-repeat;'></td>
+		<td rowspan="2" align='center' height="62">
+			<embed src="./templates/base/images/header/banner.swf" style="z-index: -1;"  wmode="opaque" height="55" width="445"></embed>
+			<!-- <object classid="clsid:d27cdb6e-ae6d-11cf-96b8-444553540000" codebase="http://fpdownload.macromedia.com/pub/shockwave/ca" width="445" title="NATO Harci Pilóta Toborzás!" align="bottom" height="55">
+                    		<param name="movie" value="./templates/base/images/header/banner.swf">
+              		<param name="quality" value="high">
+                    		<embed src="./templates/base/images/header/banner.swf" quality="high" name="nato_mini" style="z-index: -1;"  wmode="opaque" type="application/x" pluginspage="http://www.macromedia.com/go/getflashpl" title="NATO Harci Pilóta Toborzás!" align="allowScriptAccess=&quot;sameDomain&quot;" height="55" width="445">
+            		</object>    -->
+		</td>
+		<td width='148'></td>
+		</tr>
+		<tr>
+			<td bgcolor="#c6c6c6" height="18">
+				<img src="./templates/base/images/header/titles/headerBottomNews.gif" height="18" alt=""></td>
+			<td>
+				<img src="./templates/base/images/header/index_10.gif" width="20" height="18" alt=""></td></tr>
+		</table>
+		</td>
+	</tr>
+	<tr style='background-image: url(./templates/base/images/header/index_13.gif); background-repeat: repeat-x;' valign='top' height="19">
+		<td colspan='2'>
+		<table width="1000"  border="0" cellpadding="0" cellspacing="0">
+		<tr height="19">
+		<td width="148">
+			<img src="./templates/base/images/header/index_11.gif" width="148" height="19" alt=""></td>
+		<td width="22">
+			<img src="./templates/base/images/header/index_12.gif" width="22" height="19" alt=""></td>
+		<td width='669'></td>
+		<td width="13">
+			<img src="./templates/base/images/header/index_16.gif" width="13" height="19" alt=""></td>
+		<td  width="148">
+			<img src="./templates/base/images/header/index_17.gif" width="148" height="19" alt=""></td></tr>
+		</table>
+		</td>
+	</tr>
+		<tr bgcolor='#D9D9D9' valign='top' height='8'>
+		<td colspan='2'>
+		<table width="1000"  border="0" cellpadding="0" cellspacing="0">
+		<tr height="8"><td width="212">
+			<img src="./templates/base/images/header/index_18.gif" width="212" height="8" alt=""></td>
+		<td width='575'></td>
+		<td width="213">
+			<img src="./templates/base/images/header/index_21.gif" width="213" height="8" alt=""></td>
+		</tr>
+		</table>
+		</td>
+	</tr>
+</table>
+
+</td></tr><tr><td width='144' height='100%' valign='top' style='background-image: url(./templates/base/images/leftBG.gif)' align='left'><img src='./templates/base/images/poll/pollHeader.gif' alt='' title=''><table cellpadding='0' cellspacing='0' border='0' width='144'><tr><td width='135'bgcolor='#305172'><form action='redirect.php?sid=b5fcfdf7c2d78064ee81b7ce72b45be2&amp;action=saveVote' method='post'>
+<table width='100%' cellpadding='0' cellspacing='0' border='0'>
+<tr><td colspan='4' height='7'></td></tr>
+<tr>
+	<td colspan='4' class='pollText' align='center'><b>How important are custom content tools for a game?</b></td>
+</tr>
+<tr><td colspan='4' height='7'></td></tr>
+<tr>
+	<td width='2'></td>
+	<td align='right' valign='top'><input type='radio' name='pollItemID' value='165'></td>
+	<td class='pollText'>Very Important. I use Custom content tools a lot for gaming!</td>
+	<td width='2'></td>
+</tr>
+<tr><td colspan='4' height='8'></td></tr>
+<tr>
+	<td width='2'></td>
+	<td align='right' valign='top'><input type='radio' name='pollItemID' value='166'></td>
+	<td class='pollText'>Somewhat Important. It adds more variety for the community!</td>
+	<td width='2'></td>
+</tr>
+<tr><td colspan='4' height='8'></td></tr>
+<tr>
+	<td width='2'></td>
+	<td align='right' valign='top'><input type='radio' name='pollItemID' value='167'></td>
+	<td class='pollText'>Not Important. I only play with the officially released content.</td>
+	<td width='2'></td>
+</tr>
+<tr><td colspan='4' height='8'></td></tr>
+<tr>
+	<td colspan='4' align='center'><input type='submit' class='inputControl' value='Vote'></td>
+</tr>
+<tr>
+	<td colspan='4' align='center'><a href='index.php?sid=b5fcfdf7c2d78064ee81b7ce72b45be2&amp;activeMenuID=pollAllResult' class='link'>Result</a></td>
+</tr>
+<tr>
+	<td colspan='4' height='5' align='center'></td>
+</tr>
+<tr valign='bottom'>
+	<td colspan='4' height='5' align='center'><a class='newsHeader'  href='http://www.stormregion.com/forum/viewtopic.php?t=293'><b>Forum</b></a></td>
+</tr>
+</table>
+</form>
+
+</td><td width='9'><img src='./templates/base/images/box/leftSeparator.gif' alt='' title=''></td></tr><tr><td colspan='2'><img src='./templates/base/images/separator.gif' alt='' title=''></td></table><table width='100%' cellpadding='0' cellspacing='0' border='0'>
+
+<tr>
+	<td colspan='2'><img src='./templates/base/images/screenShot/header.gif' alt='' title=''></td>
+</tr>
+<tr>
+	<td bgcolor='#F4F4F4' height='5'></td>
+</tr>
+<tr>
+	<td bgcolor='#F4F4F4' align='center' valign='bottom' height='101' width='135'>
+	<a href='javascript:void(0);' onclick="window.open('pages/viewImage.php?image=admin/screenshot/304_THUMB.jpg&amp;sender=[SR]nszf','','screenx=50, screeny=50,width=1024,height=768,statusbar=0,scrollbars=1');">
+	<img src='admin/screenshot/304_THUMB.jpg' width='126' border='0' alt='' title=''></a></td>
+	<td width='9'></td>
+</tr>
+<tr>
+	<td bgcolor='#F4F4F4' align='center' valign='middle' height='21' width='135'>
+	<a href='index.php?sid=b5fcfdf7c2d78064ee81b7ce72b45be2&amp;activeMenuID=sotw' class='blackLink'>More</a>
+	<td width='9'></td>
+</tr>
+<tr><td height='1' colspan='2'><img src='./templates/base/images/separator.gif' alt='' title=''></td></tr>
+</table>
+
+</td><input type='hidden' name='activepage2' value='downloadSection.php'><input type='hidden' name='activemenuid2' value='news'><input type='hidden' name='activesubmenuid2' value='-1'><input type='hidden' name='activeprojectid2' value='-1'><input type='hidden' name='articleid2' value=''><td width='712' bgcolor='#DBDBDB' valign='top'><table cellpadding='5' cellspacing='0' border='0' bgcolor='#DBDBDB' width='100%'><tr><td><table cellpadding='4' cellspacing='0' border='0' class='simpleBlackText'>
+	<tr><td bgcolor='#5179A3' class='whiteText' colspan='2' align='center'>Downloading</td>
+	<tr bgcolor='#FFFFFF'>
+		<td>File name: </td><td>RFBWalkthrough.rar</td>
+	</tr>
+	<tr bgcolor='#E2E4E4'>
+		<td>Size: </td><td>11,69 Mb</td>
+	</tr>
+	<tr bgcolor='#FFFFFF'>
+		<td>Content type: </td><td>application/force-download</td>
+	</tr>
+	<tr>
+		<td height='15' colspan='2'></td>
+	</tr>
+	<tr>
+		<td colspan='2'><a href='index.php?sid=b5fcfdf7c2d78064ee81b7ce72b45be2&amp;activeMenuID=downloads' class='blackLink'>Back to downloads</a></td>
+	</tr>
+</table>
+<iframe frameborder='0' width='0' height='0' src='pages/downloadManager.php?fileID=183'></iframe></td></tr></table></td><td width='144' style='background-image: url(./templates/base/images/rightBG.gif)' valign='top' align='right'><table cellpadding='0' cellspacing='0' border='0' width='144'><tr><td width='9'><img src='./templates/base/images/box/rightSeparator.gif' alt='' title=''></td><td width='135'bgcolor='#4372A4'><form action='redirect.php?sid=b5fcfdf7c2d78064ee81b7ce72b45be2&amp;action=login&amp;forceRedirect=' method='post'>
+<table width='100%' cellpadding='3' cellspacing='0' border='0' class='simpleBlackText'>
+<tr>
+	<td align='center' class='simpleText'>
+		<b>Log In</b><br>
+		
+	</td>
+</tr>
+<tr>
+	<td align='center' valign='top'>
+	<input type='text' size='15' maxlength='25' name='login' class='inputControl'>
+	</td>
+</tr>
+<tr>
+	<td align='center' valign='top'>
+	<input type='password' size='15' maxlength='25' name='pass' class='inputControl'>
+	</td>
+</tr>
+<tr>
+	<td align='center'>
+	<input type='submit' value='Log In!' class='inputControl'>
+	</td>
+</tr>
+<tr>
+	<td align='center'>
+	[<a href='index.php?sid=b5fcfdf7c2d78064ee81b7ce72b45be2&amp;activeMenuID=registration' class='link'>Registration</a>]
+	</td>
+</tr>
+<tr>
+	<td align='center'>
+	[<a href='index.php?sid=b5fcfdf7c2d78064ee81b7ce72b45be2&amp;activeMenuID=forgottenPassword' class='link'>Password recovery</a>]
+	</td>
+</tr>
+</table>
+</form>
+
+</td></tr><tr><td colspan='2'><img src='./templates/base/images/separator.gif' alt='' title=''></td></table><table width='144' cellpadding='0' cellspacing='0' border='0' style='background-image: url(./templates/base/images/topTopic/bgTile.gif)'>
+<tr>
+	<td colspan='2'>
+		<img src='./templates/base/images/topTopic/header.gif' alt='' title=''>
+	</td>
+</tr>
+<tr>
+	<td width='9'></td>
+	<td width='135'>
+		<table cellpadding='5' cellspacing='0' border='0' width='100%'>
+			<tr><td bgcolor='#27415C'><a href='forum/viewtopic.php?sid=b5fcfdf7c2d78064ee81b7ce72b45be2&amp;t=113' class='simpleText'>SWINE online hos..</a></td></tr>
+			<tr><td bgcolor='#305172'><a href='forum/viewtopic.php?sid=b5fcfdf7c2d78064ee81b7ce72b45be2&amp;t=392' class='simpleText'>RTS Newbie With ..</a></td></tr>
+			<tr><td bgcolor='#27415C'><a href='forum/viewtopic.php?sid=b5fcfdf7c2d78064ee81b7ce72b45be2&amp;t=390' class='simpleText'>Configuration of..</a></td></tr>
+			<tr><td bgcolor='#305172'><a href='forum/viewtopic.php?sid=b5fcfdf7c2d78064ee81b7ce72b45be2&amp;t=396' class='simpleText'>S.W.I.N.E Map Ed..</a></td></tr>
+			<tr><td bgcolor='#27415C'><a href='forum/viewtopic.php?sid=b5fcfdf7c2d78064ee81b7ce72b45be2&amp;t=393' class='simpleText'>Vacation Time at..</a></td></tr>
+		</table>
+	</td>
+</tr>
+</table>
+
+<img src='./templates//base/images/instore.gif' alt='In stores now' title='In stores now'></td></tr><tr><td height='26' bgcolor='#4976A6' align='center' valign='middle' class='whiteText' colspan='3'><table width='1000' cellpadding='0' cellspacing='0' border='0'>
+	<tr>
+		<td width='135' style='background-image: url(./templates/base/images/footerContent1.gif)' height='26'>&nbsp;</td>
+		<td width='9'><img src='./templates/base/images/footerLeftSeparator.gif' alt='' title=''></td>
+		<td width='76' style='background-image: url(./templates/base/images/footerContent2.gif)'>&nbsp;</td>
+		<td width='11'><img src='./templates/base/images/footerBlueLeft.gif' alt='' title=''></td>
+		<td width='538' style='background-image: url(./templates/base/images/footerBlueContent.gif)' align='center' valign='middle' class='copyRight'>
+		2005 © Copyright Stormregion Ltd. All rights reserved.
+		</td>
+		<td width='11'><img src='./templates/base/images/footerBlueRight.gif' alt='' title=''></td>
+		<td width='76' style='background-image: url(./templates/base/images/footerContent2.gif)'>&nbsp;</td>
+		<td width='9'><img src='./templates/base/images/footerRightSeparator.gif' alt='' title=''></td>
+		<td width='135' style='background-image: url(./templates/base/images/footerContent1.gif)'>&nbsp;</td>
+	</tr>
+</table>
+	</td></tr></table></center></body></html>
